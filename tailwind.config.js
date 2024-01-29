d03eaf6163959534400fa6787b52293d3e922b46
jisaumyaji <patelsaumya705435@gmail.com>
2024-01-29 11:22:38 +0530

    changes done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      animation: {
-        wiggle: ' 0% { transform: translateY(0)} 100% { transform: translateY(-100vh)}',
-      }
+      keyframes: {
+         propel: {
+          '100%': { transform: 'translateY(0)',opacity:'1' },
+          '80%': { transform: 'translateY(25%)',opacity:'0.9' },
+          '50%': { transform: 'translateY(35%)',opacity:'0.6' },
+          '20%': { transform: 'translateY(55%)',opacity:'0.3' },
+          '0%': { transform: 'translateY(105%)',opacity:'0' },
+        },
+      },
     },
   },
   plugins: [],
